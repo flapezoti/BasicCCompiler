0e329daad28f1643b4189fb8c1573a7bfb23f6f8
Flavia Pezoti <pezoti@amazon.com>
2017-09-01 16:29:28 -0300

    Adding command line parser

---
+++ src/com/company/CommandLineHelper.java
@@ -0,0 +1,65 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.Parser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.ParseException;
+
+public class CommandLineHelper {
+
+    public static Map<String,String> getCommandLineArgsAsMap(String[] args){
+        final Options options
+                = new Options();
+
+        if (args == null) {
+            throw new IllegalArgumentException("Command Line args can't be null");
+        }
+
+        final String arquivoSimbolosArg = "arquivoSimbolos";
+        final Option arquivoSimbolosOption = new Option( arquivoSimbolosArg, true, "Caminho do arquivo com os símbolos terminais da linguagem");
+        arquivoSimbolosOption.setRequired( true );
+        options.addOption(arquivoSimbolosOption);
+
+        final String arquivoTSTArg = "arquivoTST";
+        final Option arquivoTSTOption = new Option( arquivoTSTArg, true, "Caminho para arquivo binário de saída da tabela de simbolos terminais");
+        arquivoTSTOption.setRequired( true );
+        options.addOption(arquivoTSTOption);
+
+        final String arquivoLogArg = "arquivoLog";
+        final Option arquivoLogOption = new Option( arquivoLogArg, true, "Caminho para arquivo texto de saída dos logs da aplicação");
+        arquivoLogOption.setRequired( true );
+        options.addOption(arquivoLogOption);
+
+        final String arquivoTesteArg = "arquivoTeste";
+        final Option arquivoTesteOption = new Option( arquivoTesteArg, true, "Caminho para o arquivo texto de teste");
+        arquivoTesteOption.setRequired( true );
+        options.addOption(arquivoTesteOption);
+
+
+        final Parser parser = new GnuParser();
+        final CommandLine commandLine;
+        try {
+            commandLine = parser.parse(options, args);
+        } catch ( ParseException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+
+        if (!commandLine.getArgList().isEmpty()) {
+            throw new IllegalArgumentException("There were non-recognized options/arguments: " + commandLine.getArgList());
+        }
+
+        Map<String,String> cmdArgsMap = new HashMap<String, String>();
+        cmdArgsMap.put(arquivoSimbolosArg, commandLine.getOptionValue(arquivoSimbolosArg));
+        cmdArgsMap.put(arquivoLogArg, commandLine.getOptionValue(arquivoLogArg));
+        cmdArgsMap.put(arquivoTesteArg, commandLine.getOptionValue(arquivoTesteArg));
+        cmdArgsMap.put(arquivoTSTArg, commandLine.getOptionValue(arquivoTSTArg));
+
+        return cmdArgsMap;
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,19 @@
 package com.company;
 
+
+import org.apache.commons.cli.Option;
+
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        Map cmdLineArgsMap = CommandLineHelper.getCommandLineArgsAsMap(args);
+
+        /* TODO
+            Ler o arquivo com os símbolos a serem inseridos e para cada simbolo chamar a função search and insert da TST
+         */
 
     }
 }
