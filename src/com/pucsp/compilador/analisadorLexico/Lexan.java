eae7fb3da4cd1eb7c914429ec1d33b1222c7f2c1
Flavia Pezoti <pezoti@amazon.com>
2017-10-20 21:01:59 -0200

    Implementing singleton on Lexan

--- src/com/pucsp/compilador/analisadorLexico/Lexan.java
+++ src/com/pucsp/compilador/analisadorLexico/Lexan.java
@@ -1,4 +1,21 @@
 package com.pucsp.compilador.analisadorLexico;
 
 public class Lexan {
+    private static Lexan instance;
+    String x;
+
+    private Lexan(String x){
+        x = x;
+    }
+
+    public void create(String x){
+       this.instance = new Lexan(x);
+    }
+
+    static public Lexan getInstance(){
+        return instance;
+    }
+
+
+
 }
