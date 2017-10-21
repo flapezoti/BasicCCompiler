fe5e74e2fcb267e7ab0c08345ed31b926a21dc94
Flavia Pezoti <pezoti@amazon.com>
2017-10-21 03:09:51 -0200

    Criando o token

---
+++ src/com/pucsp/compilador/analisadorLexico/AEF.java
@@ -0,0 +1,4 @@
+package com.pucsp.compilador.analisadorLexico;
+
+public class AEF {
+}
--- src/com/pucsp/compilador/analisadorLexico/Lexan.java
+++ src/com/pucsp/compilador/analisadorLexico/Lexan.java
@@ -1,21 +1,34 @@
 package com.pucsp.compilador.analisadorLexico;
 
+import com.pucsp.compilador.tabelaSimboloTerminal.TST;
+import com.pucsp.compilador.tabelaSimboloTerminal.TokensEspeciais;
+
 public class Lexan {
-    private static Lexan instance;
-    String x;
+    private TST tst;
 
-    private Lexan(String x){
-        x = x;
+
+    public Token montarToken(String cadeia, TokensEspeciais tipoToken) throws Exception {
+        Token token = new Token();
+        int indiceTST = tst.searchInsert(cadeia, 'C', Boolean.TRUE);
+
+        if (indiceTST != -1){
+            //Significa que o token é uma palavra reservada
+            token.setConteudoNaTST(cadeia);
+
+        } else {
+            //Pode ser NUMBER FLOAT ALPHA ou IDENT
+            indiceTST = tst.buscaSimboloEspecial(tipoToken.name());
+            token.setConteudoNaTST(tipoToken.name());
+
+            if ( tipoToken.equals(TokensEspeciais.NUMBER)){
+                int valorInteiro = Integer.parseInt(cadeia);
+                token.setValorNumericoInt(valorInteiro);
+            } else if (tipoToken.equals(TokensEspeciais.FLOAT)){
+                float valorFloat = Float.parseFloat(cadeia);
+                token.setValorNumericoFloat(valorFloat);
+            }
+        }
+        token.setItemLexico(cadeia);
+        token.setIndiceNaTST(indiceTST);
     }
-
-    public void create(String x){
-       this.instance = new Lexan(x);
-    }
-
-    static public Lexan getInstance(){
-        return instance;
-    }
-
-
-
 }
